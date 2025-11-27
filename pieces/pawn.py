5141b17033d425e8ab9fb7a62e53b359aad1c720
nedyah212 <hploszay2@academic.rrc.ca>
2025-11-27 11:39:39 -0600

    pawn saves its starting location in order to allow double move at before initial move

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -2,6 +2,10 @@ from pieces._piece import Piece
 
 class Pawn(Piece):
 
+  def __init__(self):
+    self.team_to_col = {0: 1, 1: 6}
+    self.start_row = self.team_to_col[self.team]
+
   def move(self):
     pass
 
